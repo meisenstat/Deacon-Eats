f076c754ab1a6b0246084392009a084ebc95d1de
Ben <benfolds16@gmail.com>
2019-03-18 00:07:06 -0400

    button updates

--- Deacon Eats/Remove_ListingTableView.swift
+++ Deacon Eats/Remove_ListingTableView.swift
@@ -14,7 +14,17 @@ import UIKit
 class Remove_ListingTableView: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        let button = UIButton(frame: CGRect(x: 100, y: 100, width: 100, height: 50))
+        button.backgroundColor = .green
+        button.setTitle("Test Button", for: .normal)
+        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
         
+        self.view.addSubview(button)
+    }
+    
+    @objc func buttonAction(sender: UIButton!) {
+        print("Button tapped")
+    }
        
 }
-}
+
