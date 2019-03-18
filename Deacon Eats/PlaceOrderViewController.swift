41b029785eba0f447456a3f5184cc211fe5b28b3
Mitch Eisenstat <mitcheisenstat@Mitchs-MBP.dhcp.wfu.edu>
2019-03-18 17:03:46 -0400

    final commit sprint 1

--- Deacon Eats/PlaceOrderViewController.swift
+++ Deacon Eats/PlaceOrderViewController.swift
@@ -9,12 +9,13 @@
 import Foundation
 import UIKit
 
-class PlaceOrderViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
-    
+class PlaceOrderViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextViewDelegate {
+
     @IBOutlet weak var destinationButton: UIButton!
     @IBOutlet weak var timeLimitButton: UIButton!
     @IBOutlet weak var otherButton: UIButton!
     @IBOutlet weak var navigationBar: UINavigationBar!
+    @IBOutlet weak var submitButton: UIButton!
     
     var destinations = ["Luter", "Babcock", "Johnson", "Bostwick", "South", "Angeleau", "Collins", "Kitchen", "Poteat", "Davis", "Taylor", "Piccolo", "Palmer", "Huffman", "Efrid", "Dogwood", "Magnolia", "Polo", "Martin", "NCA"]
     var timeOptions = [[0, 1, 2, 3, 4, 5, 6], [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55]]
@@ -34,6 +35,8 @@ class PlaceOrderViewController: UIViewController, UIPickerViewDelegate, UIPicker
     
     override func viewDidLoad() {
         super.viewDidLoad()
+//        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: .UIResponder.keyboardWillShowNotification, object: nil)
+        setUpObserver()
         setupGeneral()
         setupDestinations()
         setupTimes()
@@ -43,6 +46,30 @@ class PlaceOrderViewController: UIViewController, UIPickerViewDelegate, UIPicker
         view.addSubview(pickerView)
         view.addSubview(otherView)
         print("rest: ", restaurant)
+    }
+    
+    private func setUpObserver() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    @objc fileprivate func keyboardWillShow(notification:NSNotification) {
+        if let keyboardRectValue = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            let keyboardHeight = keyboardRectValue.minY
+            print("height: ", keyboardHeight)
+            print("diff", keyboardHeight - otherButton.frame.maxY)
+            
+//            otherView = UIView(frame: CGRect(x: 0, y: otherButton.frame.maxY + 20, width: view.frame.width, height: keyboardHeight - otherButton.frame.maxY - 44))
+            print("frame before: ", otherView.frame)
+            otherView.frame = CGRect(x: 0, y: otherButton.frame.maxY + 20, width: view.frame.width, height: keyboardHeight - otherButton.frame.maxY - 44)
+            print("frame after: ", otherView.frame)
+            otherTextView.frame = CGRect(x: 0, y: 44, width: self.view.frame.size.width, height: otherView.frame.size.height-44)
+//            dismissText()
+        }
+    }
+
+    @objc func keyboardWillHide(notification: Notification) {
+        // keyboard is dismissed/hidden from the screen
     }
     
     func setupGeneral() {
@@ -61,10 +88,16 @@ class PlaceOrderViewController: UIViewController, UIPickerViewDelegate, UIPicker
         pickerView = UIView(frame: CGRect(x: 0, y: view.frame.height - 260, width: view.frame.width, height: 260))
         pickerView.isHidden = true
         
+        
         // set up other parent view
+        
+//        var keyboardSize = notification.userInfo(valueForKey(UIKeyboardFrameBeginUserInfoKey))
+//        print(keyboardSize)
+        
 //        otherView = UIView(frame: CGRect(x: 0, y: view.frame.height - 330, width: view.frame.width, height: 330))
         print(otherButton.frame.maxY)
-        otherView = UIView(x: 0, y: CGFloat(otherButton.frame.maxY) + CGFloat(20), width: view.frame.width, height: 100)
+//        otherView = UIView(frame: CGRect(x: 0, y: otherButton.frame.maxY + 20, width: view.frame.width, height: 300))
+        otherView = UIView(frame: CGRect(x: 0, y: otherButton.frame.maxY + 20, width: view.frame.width, height: 300))
         otherView.isHidden = true
         
         
@@ -103,16 +136,28 @@ class PlaceOrderViewController: UIViewController, UIPickerViewDelegate, UIPicker
     }
     
     func setupOther() {
+        otherTextView.delegate = self
         otherTextView.text = "Enter any requests you have that the deliverer should know before accepting your order"
         otherTextView.center = CGPoint(x: view.frame.size.width  / 2, y: view.frame.size.height / 2)
         otherTextView.frame = CGRect(x: 0, y: 44, width: self.view.frame.size.width, height: otherView.frame.size.height-44)
         otherTextView.backgroundColor = UIColor.lightGray
         otherTextView.keyboardAppearance = UIKeyboardAppearance.dark
         otherTextView.keyboardType = UIKeyboardType.asciiCapable
+        otherTextView.isEditable = true
+        
+        otherTextView.returnKeyType = .done
         
         otherView.addSubview(otherTextView)
         
         otherButton.addTarget(self, action: #selector(PlaceOrderViewController.otherClicked), for: UIControl.Event.touchUpInside)
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if (text as NSString).rangeOfCharacter(from: CharacterSet.newlines).location == NSNotFound {
+            return true
+        }
+        otherTextView.resignFirstResponder()
+        return false
     }
     
     func dismissPicker() {
@@ -173,6 +218,7 @@ class PlaceOrderViewController: UIViewController, UIPickerViewDelegate, UIPicker
     }
     
     @objc func dismissKeyboard() {
+        otherTextView.resignFirstResponder()
         otherInstructions = otherTextView.text
         otherButton.setTitle("Edit instructions", for: .normal)
         otherView.isHidden = true
@@ -235,3 +281,7 @@ class PlaceOrderViewController: UIViewController, UIPickerViewDelegate, UIPicker
 
     
 }
+
+//private extension Selector {
+//    static let keyboardWillShow = #selector(PlaceOrderViewController.keyboardWillShow(notification:))
+//}
