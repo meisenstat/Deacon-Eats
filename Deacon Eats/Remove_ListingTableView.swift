9026757cd4767f1c6a6dd79e1b7be60941522de6
Ben <benfolds16@gmail.com>
2019-03-22 12:19:42 -0400

    updates 2

--- Deacon Eats/Remove_ListingTableView.swift
+++ Deacon Eats/Remove_ListingTableView.swift
@@ -12,19 +12,58 @@ import UIKit
 
 
 class Remove_ListingTableView: UITableViewController {
+    
+    var button = UIButton()
+    var listings = ["data", "from", "database"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        let button = UIButton(frame: CGRect(x: 100, y: 100, width: 100, height: 50))
-        button.backgroundColor = .green
-        button.setTitle("Test Button", for: .normal)
-        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        
+        
+    
+        button = UIButton(frame: CGRect(x: 300, y: 100, width: 100, height: 50))
+        button.backgroundColor = .blue
+        button.setTitle("Pick-Up", for: .normal)
+        //button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        
         
         self.view.addSubview(button)
+        
+        self.tableView.rowHeight = 50
+        //self.tableView.tableFooterView = UIView()
+        //self.tableView.separatorStyle = UITableViewCell.SeparatorStyle.none
+        
     }
     
-    @objc func buttonAction(sender: UIButton!) {
-        print("Button tapped")
+    
+    
+    func addTarget(_ target: UIView?, action: Selector, for controlEvents: UIControl.Event) {
+        
     }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if(self.listings.count == 0){
+            return 1
+        }
+        return self.listings.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "locationCell", for: indexPath)
+        cell.textLabel?.text = self.listings[indexPath.row]
+        if (indexPath.row % 2 == 0) {
+            cell.backgroundColor = UIColor(red: 231/255, green: 231/255, blue: 231/255, alpha: 1.0)
+        }
+        return cell
+    }
+
+   /* @objc func buttonAction(sender: UIButton!) {
+        
+        
+        print("Button tapped")
+        
+    } */
        
 }
 
--- Deacon Eats/ViewController.swift
+++ Deacon Eats/ViewController.swift
@@ -13,19 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let button = UIButton(frame: CGRect(x: 100, y: 100, width: 100, height: 50))
-        button.backgroundColor = .green
-        button.setTitle("Test Button", for: .normal)
-        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
         
-        self.view.addSubview(button)
-    }
-    
-    @objc func buttonAction(sender: UIButton!) {
-        print("Button tapped")
-    
-    }
-
 
 }
 
+}
