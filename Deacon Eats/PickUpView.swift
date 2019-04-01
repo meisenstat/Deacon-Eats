fd0cea1a9517672c07a422fec92287eb17ab54b0
Ben <benfolds16@gmail.com>
2019-04-01 17:27:04 -0400

    a little code working

--- Deacon Eats/PickUpView.swift
+++ Deacon Eats/PickUpView.swift
@@ -17,13 +17,20 @@ class PickUpView: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        pickUpButton = UIButton(frame: CGRect(x: 300, y: 100, width: 100, height: 50))
+        pickUpButton = UIButton(frame: CGRect(x:100, y: 500, width: 100, height: 50))
+        pickUpButton.center = self.view.center
         pickUpButton.backgroundColor = .blue
         pickUpButton.setTitle("Pick-Up", for: .normal)
         
         self.view.addSubview(pickUpButton)
-}
+        
+        
+        
+       
+    }
     
     
 }
+    
+
 
