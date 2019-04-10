3c6b60fde6d45cb3df8ab51b23e4d1a01b81aa94
Mitch Eisenstat <mitcheisenstat@Mitchs-MacBook-Pro.local>
2019-04-10 17:22:28 -0400

    merging changes for listings update

--- Deacon Eats/ListingsViewController.swift
+++ Deacon Eats/ListingsViewController.swift
@@ -39,41 +39,10 @@ class listing {
 }
 
 
-//public class MultiColumnCell : UITableViewCell
-//{
-//    public static String ID = "MultiColumnCell";
-//
-//    public UILabel LeftValue { get set }
-//    public UILabel RightValue { get set}
-//
-//    public MultiColumnCell()
-//{
-//    LeftValue = new UILabel(new RectangleF(0, 0, 150, 50));
-//    RightValue = new UILabel(new RectangleF(150, 0, 150, 50));
-//    LeftValue.BackgroundColor = UIColor.Red;
-//    RightValue.BackgroundColor = UIColor.Green;
-//
-//    AddSubview(LeftValue);
-//    AddSubview(RightValue);
-//    }
-//
-//}
-
-
-
 class ListingsViewController: UITableViewController {
     
-//    var scrollView: UIScrollView!
-//    var imageView: UIImageView!
     var listOfListings : [listing] = []
-    //var data = [CellData]()
-    
-    
-    //override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    
-    
-    //override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    
+    var selected = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -93,52 +62,26 @@ class ListingsViewController: UITableViewController {
         let headerView: UIView = UIView.init(frame: CGRect(x:1, y:50, width:276, height:44))
         headerView.backgroundColor = UIColor(red: 235/255.0, green: 235/255.0, blue: 235/255.0, alpha: 1.0)
         
-        
-        //let labelView: UILabel = UILabel.init(frame: CGRect(x:4, y:5, width:276, height:24))
-        //labelView.text = "hello"
         let usernameButton = UIButton()
         usernameButton.frame = CGRect(x: 0, y: 0, width: 70, height: 44)
         usernameButton.setTitle("Username", for: .normal)
         usernameButton.setTitleColor(UIColor.black, for: .normal)
         usernameButton.titleLabel?.font = UIFont.systemFont(ofSize: 14)
-        //headerView.addSubview(usernameButton)
-        
         
         let pickupButton = UIButton()
         pickupButton.frame = CGRect(x: usernameButton.frame.maxX, y: 0, width: 70, height: 44)
         pickupButton.setTitle("Pick Up", for: .normal)
         pickupButton.setTitleColor(UIColor.black, for: .normal)
         pickupButton.titleLabel?.font = UIFont.systemFont(ofSize: 14)
-        //headerView.addSubview(pickupButton)
-        
         
         let dropoffButton = UIButton()
         usernameButton.frame = CGRect(x: pickupButton.frame.maxX, y: 0, width: 70, height: 44)
         dropoffButton.setTitle("Drop Off", for: .normal)
         dropoffButton.setTitleColor(UIColor.black, for: .normal)
         dropoffButton.titleLabel?.font = UIFont.systemFont(ofSize: 14)
-        //headerView.addSubview(dropoffButton)
-        
-        /*
-        let expireTimeButton = UIButton()
-        expireTimeButton.frame = CGRect(x: 150, y: 0, width: 50, height: 44)
-        expireTimeButton.setTitle("", for: .normal)
-        expireTimeButton.setTitleColor(UIColor.black, for: .normal)
-        headerView.addSubview(expireTimeButton)
- */
-        
-     
         
         self.tableView.tableHeaderView = headerView
         
-        
-//        scrollView = UIScrollView(frame: view.bounds)
-//        //scrollView.backgroundColor = UIColor.blackColor()
-//        scrollView.contentSize = imageView.bounds.size
-//        //scrollView.autoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight
-//
-//        scrollView.addSubview(imageView)
-//        view.addSubview(scrollView)
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -152,14 +95,24 @@ class ListingsViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     
         let cell = tableView.dequeueReusableCell(withIdentifier: "item", for: indexPath)
-//        cell.label1.text = self.listOfListings[indexPath.row].userName
-//        cell.label2.text = self.listOfListings[indexPath.row].pickUp
-//        cell.label3.text = [NSString stringWithFormat:@"End %ld", (long)indexPath.row];
+        
         cell.textLabel?.text = self.listOfListings[indexPath.row].userName + "  |   " + self.listOfListings[indexPath.row].pickUp +  "  |   " + self.listOfListings[indexPath.row].dropOff + "  |   " + self.listOfListings[indexPath.row].expireTime
         return cell
     }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("row: " + String(indexPath.row))
+        selected = listOfListings[indexPath.row].userName + "  |   " + self.listOfListings[indexPath.row].pickUp +  "  |   " + self.listOfListings[indexPath.row].dropOff + "  |   " + self.listOfListings[indexPath.row].expireTime
+        
+        self.performSegue(withIdentifier: "pickUpScreen", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if(segue.identifier == "pickUpScreen") {
+            print("sel: " + selected)
+            let newVC = segue.destination as! PickUpView
+            newVC.selected = selected
+        }
+    }
+    
 }
-
-
-// + self.listOfListings[indexPath.row].pickUp + self.listOfListings[indexPath.row].dropOff + Formatter.string(self.listOfListings[indexPath.row].expireTime)
